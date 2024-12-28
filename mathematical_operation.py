8cb96c949aab62c03fbbc847899bdc68fed5e954
Ashmit359 <ashmit.sinha359@gmail.com>
2024-12-28 13:08:17 +0530

    if we have two d array

---
+++ mathematical_operation.py
@@ -0,0 +1,18 @@
+#g=a-b
+# np.subtract (a, b)
+# b+an
+# p.add (b, a)
+# a/ b
+# np.divide(a,b)
+# a*b
+# np.multiply (a, b)
+# np.exp(b)
+# np.sqrt(b)
+# np.pow(a)
+
+import numpy as np 
+
+arr1 = np.array ([[30,54],[55,85]])
+arr2 = np.array ([[54,88],[64,41]])
+print (arr1 + arr2)
+print (np.add(arr1,arr2))
