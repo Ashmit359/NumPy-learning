5fcbbb0fffb24b69d324c28db3fd02bb382f734c
Ashmit359 <ashmit.sinha359@gmail.com>
2024-12-28 19:03:15 +0530

    join & split

---
+++ join_&_split_array.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+# a = [4,5,6,7]
+# b = [8,9,10,11]
+# print(type(a))
+
+#Concatenate
+# arr1 = np.array([45,65,84])
+# arr2 = np.array([54,8,55])
+# print(np.concatenate([arr1,arr2]))
+
+arr1 = np.array([[45,65,84],[54,85,98]])
+arr2 = np.array([[54,8,55],[54,98,54]])
+print(np.concatenate([arr1,arr2], axis = 0))
+print(np.concatenate([arr1,arr2], axis = 1))
+print(np.hstack([arr1,arr2])) #horizontal concatention
+print(np.vstack([arr1,arr2])) #vertical concatention
+
+a = np.array([4,54,6,8,2,58])
+b = np.array_split(a,3)
+print(b[1])
--- mathematical_operation.py
+++ mathematical_operation.py
@@ -16,3 +16,24 @@ arr1 = np.array ([[30,54],[55,85]])
 arr2 = np.array ([[54,88],[64,41]])
 print (arr1 + arr2)
 print (np.add(arr1,arr2))
+
+arr1 = np.array ([[30,54],[55,85]])
+arr2 = np.array ([[54,88],[64,41]])
+print (arr1 - arr2)
+print (np.subtract (arr1,arr2))
+
+arr1 = np.array ([[30,54],[55,85]])
+arr2 = np.array ([[54,88],[64,41]])
+print (arr1 / arr2)
+print (np.divide (arr1,arr2))
+
+arr1 = np.array ([[30,54],[55,85]])
+arr2 = np.array ([[54,88],[64,41]])
+print (arr1 * arr2)
+print (np.multiply (arr1,arr2))
+
+arr1 = np.array ([[30,54],[55,85]])
+print (np.exp(arr1) )
+
+arr1 = np.array ([[3,4],[5,6]])
+print (np.sqrt(arr1) )
