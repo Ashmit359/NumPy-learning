f7796ea76287299dbabd0508ba76e11894578bba
Ashmit359 <ashmit.sinha359@gmail.com>
2024-12-28 11:41:26 +0530

    array slicing

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import numpy as np 
 
-arr = np.array([[10,20,30,40],[52,68,89,25]])
-print(arr)
-print(type(arr))
+arr = np.array([10,20,30,40])
+print (arr[0:3])
+print (arr[0:])
+print (arr[3:])
+print (arr[-1:3])
+print (arr[:-1])
+print (arr[:-2])
+print (arr[:-3])
+print (arr[:-4])
+print (arr[-1:])
+print (arr[-2:1])
+print (arr[-3:2])
