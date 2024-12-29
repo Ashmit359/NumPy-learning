9019ca7b86ef14479c2a5ca209899364cb6491c6
Ashmit359 <ashmit.sinha359@gmail.com>
2024-12-29 13:13:56 +0530

    searchsorted use in sorted array only

--- search_sort_filter.py
+++ search_sort_filter.py
@@ -3,7 +3,11 @@ import numpy as np
 # arr = np.array([[54,8,9,7,65,54],[54,98,3,88,7655,84]])
 # print(np.sort(arr))
 
-arr = np.array ([54,6,8,98,32])
-s = np.where(arr == 6)
-p = np.where(arr % 4 == 0)
-print(s,p)
+# arr = np.array ([54,6,8,98,32])
+# s = np.where(arr == 6)
+# p = np.where(arr % 4 == 0)
+# print(s,p)
+
+arr = np.array ([1,2,3,4,5,6,7,8,9])
+s = np.searchsorted(arr,5)
+print(s)
