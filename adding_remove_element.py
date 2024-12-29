8a60e8e9c41d5c32b22d50eab1c042665c9bd057
Ashmit359 <ashmit.sinha359@gmail.com>
2024-12-29 12:17:07 +0530

    add element in last

---
+++ adding_remove_element.py
@@ -0,0 +1,8 @@
+#np.append (h,g) -append items to an array
+#np.insert (a, 1, 5) -inserta items in an array
+#np.delete (a, [1]) - delete items from an array
+
+import numpy as np 
+
+a = np.array([45,65,54,55])
+print(np.append(a,90))
