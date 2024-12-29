aafe39fd4a3cfa4e58a6f0e3d3d9b4a11597e327
Ashmit359 <ashmit.sinha359@gmail.com>
2024-12-29 12:08:34 +0530

    add split

--- join_&_split_array.py
+++ join_&_split_array.py
@@ -9,13 +9,13 @@ import numpy as np
 # arr2 = np.array([54,8,55])
 # print(np.concatenate([arr1,arr2]))
 
-arr1 = np.array([[45,65,84],[54,85,98]])
-arr2 = np.array([[54,8,55],[54,98,54]])
-print(np.concatenate([arr1,arr2], axis = 0))
-print(np.concatenate([arr1,arr2], axis = 1))
-print(np.hstack([arr1,arr2])) #horizontal concatention
-print(np.vstack([arr1,arr2])) #vertical concatention
+# arr1 = np.array([[45,65,84],[54,85,98]])
+# arr2 = np.array([[54,8,55],[54,98,54]])
+# print(np.concatenate([arr1,arr2], axis = 0))
+# print(np.concatenate([arr1,arr2], axis = 1))
+# print(np.hstack([arr1,arr2])) #horizontal concatention
+# print(np.vstack([arr1,arr2])) #vertical concatention
 
 a = np.array([4,54,6,8,2,58])
-b = np.array_split(a,3)
-print(b[1])
+b= np.array_split(a,3)
+print(b[3])
