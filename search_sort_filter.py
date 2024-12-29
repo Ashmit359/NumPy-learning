e369c89318227afa48a89b185ec37a07d265add6
Ashmit359 <ashmit.sinha359@gmail.com>
2024-12-29 13:11:38 +0530

    search element in array

--- search_sort_filter.py
+++ search_sort_filter.py
@@ -1,4 +1,9 @@
 import numpy as np
 
-arr = np.array([[54,8,9,7,65,54],[54,98,3,88,7655,84]])
-print(np.sort(arr))
+# arr = np.array([[54,8,9,7,65,54],[54,98,3,88,7655,84]])
+# print(np.sort(arr))
+
+arr = np.array ([54,6,8,98,32])
+s = np.where(arr == 6)
+p = np.where(arr % 4 == 0)
+print(s,p)
